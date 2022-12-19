05b491610d0ed4fb35ec4520c9f0926c2a506114
Susanna Anil <55010363+sanil72900@users.noreply.github.com>
2022-12-19 06:33:40 +0000

    source code in preprocess

--- 20_code/process_text.py
+++ 20_code/process_text.py
@@ -7,7 +7,13 @@ import re
 filename = "../10_cleaned_data/generic_sentiment_dataset_50k_cleaned.csv"
 data = pd.read_csv(filename)
 
-# source clean data code: https://www.kaggle.com/code/akgeni/logistic-regression-with-countvectorizer
+# source clean data code:
+''' 
+These functions were sourced from another author. Contains basic functions to clean raw text data for tokenization using regex.
+See original code here: https://www.kaggle.com/code/akgeni/logistic-regression-with-countvectorizer
+'''
+
+
 def remove_unwanted_text(text):
     if text.startswith("Re: "):
         text = text.replace("Re:", "")
@@ -19,23 +25,28 @@ def remove_unwanted_text(text):
     new_text = re.sub(r"[^\w\s]+", " ", new_text)
     return new_text
 
+
 def remove_link(text):
     new_text = re.sub(r"http\S+", " ", text)
     new_text = re.sub(r"www.\S+", " ", new_text)
     return new_text
+
 
 def remove_mentions(text):
     new_text = re.sub(r"@\S+", " ", text)
     new_text = re.sub(r"RT[\s]+", " ", new_text)
     return new_text
 
+
 def remove_html(text):
     return re.sub(r"<[^<>]+>", " ", text)
+
 
 def clean_text(data):
     pool = multiprocessing.Pool(processes=4)
     data["clean_text"] = pool.map(remove_unwanted_text, data["text"])
     return data
+
 
 clean_data = clean_text(data)
 clean_data.to_csv("../10_cleaned_data/processed_text.csv")
