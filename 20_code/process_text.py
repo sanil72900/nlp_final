e2dba7137f84b234b264c1bfedf12c69148accf0
Susanna Anil <55010363+sanil72900@users.noreply.github.com>
2022-12-19 06:29:18 +0000

    update README

--- 20_code/process_text.py
+++ 20_code/process_text.py
@@ -4,12 +4,11 @@ import numpy as np
 import multiprocessing
 import re
 
-data = pd.read_csv("../10_cleaned_data/generic_sentiment_dataset_50k_cleaned.csv")
-
+filename = "../10_cleaned_data/generic_sentiment_dataset_50k_cleaned.csv"
+data = pd.read_csv(filename)
 
 # source clean data code: https://www.kaggle.com/code/akgeni/logistic-regression-with-countvectorizer
 def remove_unwanted_text(text):
-
     if text.startswith("Re: "):
         text = text.replace("Re:", "")
     new_text = text.replace("\n", ". ")
@@ -20,30 +19,23 @@ def remove_unwanted_text(text):
     new_text = re.sub(r"[^\w\s]+", " ", new_text)
     return new_text
 
-
 def remove_link(text):
     new_text = re.sub(r"http\S+", " ", text)
     new_text = re.sub(r"www.\S+", " ", new_text)
     return new_text
-
 
 def remove_mentions(text):
     new_text = re.sub(r"@\S+", " ", text)
     new_text = re.sub(r"RT[\s]+", " ", new_text)
     return new_text
 
-
 def remove_html(text):
     return re.sub(r"<[^<>]+>", " ", text)
-
 
 def clean_text(data):
     pool = multiprocessing.Pool(processes=4)
     data["clean_text"] = pool.map(remove_unwanted_text, data["text"])
     return data
 
-
 clean_data = clean_text(data)
-
-
 clean_data.to_csv("../10_cleaned_data/processed_text.csv")
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# nlp_final
+# Comparison of Generative and Discriminative Language Models for Sentiment Analysis of Product Reviews
+
+## Setup instructions
+1. Install all packages specified in requirements.txt
+
+2. To clean and process raw data, open process_text.py and run script
+
+3. To build Naive Bayes (generative) model, open naivebayes.py and run script
+
+4. To build bidirectional LSTM (discriminative) model, open bi-rnn.py and run script
